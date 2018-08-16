a9653c8b3f992c12581a7a70275b4c4b2feea750
Mike Bryant <mike.bryant.jr@icloud.com>
2018-08-16 15:42:30 -0600

    first css branch commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const movie_SEARCH_URL = 'http://www.omdbapi.com?apikey=389ac1fa&';
 const YOUTUBE_Trailer_URL = 'https://www.googleapis.com/youtube/v3/search';
+const YOUTUBE_MOVIE_URL = 'https://www.googleapis.com/youtube/v3/channels';
 
 let query = '';
 ///////////////////Get Initial search list of movies///////////////////////////
@@ -31,62 +32,38 @@ function displayMovies(data) {
 }
 
 function returnResults(movie) {
-	return `
-          <div class="column">
-                <img src="${movie.Poster}" class="img-list">
-                <h3>${movie.Title}</h3>
-								<a onclick="movieSelected('${movie.imdbID}')" class="btn" href="#">Movie Details</a>
-								<a onclick="youtubeTrailer('${movie.Title} Trailer')" class="btn" href="#">Watch Trailer</a>
-          </div>
+	return `					
+					<div class="col-4">
+						<div class="card">
+						<a class="card-image" onclick="movieSelected('${
+							movie.imdbID
+						}')" href="#"><img src="${movie.Poster}"></a>
+							<div class="card-content">
+							<h3>${movie.Title}</h3>
+							<a onclick="movieSelected('${
+								movie.imdbID
+							}')" class="btn movieDetailScreen" href="#">Movie Details</a>
+							<a onclick="youtubeTrailer('${
+								movie.Title
+							} Trailer')" class="btn trailer" href="#">Watch Trailer</a>
+							</div>
+						</div>
+					</div>
     `;
 }
-
-function youtubeTrailer(title) {
-	console.log(title);
-	sessionStorage.setItem('trailerText', title); //stores id locally in application tab session storage
-	window.location = 'movie.html';
-
-	return false;
-}
-
-function getTrailer() {
-	let trailer = sessionStorage.getItem('trailerText');
-	const trailerQuery = {
-		part: 'snippet',
-		key: 'AIzaSyDbDK_yKzFivQBrkukDw3lwBVsFgdDUmNY',
-		// AIzaSyBiFe0gSq_xIil6nwieRBdNbOuaI69AFnk created new key
-		q: trailer
-	};
-	$.getJSON(YOUTUBE_Trailer_URL, trailerQuery, trailerLink);
-}
-
-function trailerLink(data) {
-	console.log('test20', data);
-	const detailOfTrailer = trailerResults(data);
-	$('#movie').html(detailOfTrailer);
-	console.log('test8', detailOfTrailer);
-}
-
-function trailerResults(trailer) {
-	let trailerDetail = trailer;
-	console.log('test22', trailerDetail);
-	return `
-<div class="detail-row">
-<img src="${trailerDetail}" class="thumbnail">
-</div>`;
-}
-
 
 ///////////////////Get Detail of selected movie///////////////////////////
 
 function movieSelected(id, title) {
-	console.log(id, title);
+	console.log(id, title, 'test1');
 	sessionStorage.setItem('movieId', id); //stores id locally in application tab session storage
+	sessionStorage.setItem('movieTitle', title);
 	window.location = 'movie.html';
 	return false;
 }
 
 function getMovieDetails() {
+	console.log('get movie');
 	let movieId = sessionStorage.getItem('movieId');
 	const query = {
 		i: movieId
@@ -95,11 +72,13 @@ function getMovieDetails() {
 }
 
 function detailedMovie(data) {
+	console.log('get movie detail');
 	const detailOfMovie = movieDetailResults(data);
 	$('#movie').html(detailOfMovie);
 }
 
 function movieDetailResults(movie) {
+	console.log('get movie results');
 	let movieDetail = movie;
 	return `
 <div class="detail-row">
@@ -153,20 +132,100 @@ function movieDetailResults(movie) {
 		</ul>
 		<hr>
 	</div>
-	<div>
+	<div class="btn-group">
 	<a href="http://imdb.com/title/${
-			movieDetail.imdbID
-		}" target="_blank" class="btn">View IMDB</a>
-    <a href="index.html" class="btn">Back To Search</a>
-    <a href="#" id="watchNow" class="btn btn-red">Watch Now</a></div>
+		movieDetail.imdbID
+	}" target="_blank" class="btn">View IMDB</a>
+		<a href="index.html" class="btn">Back To Search</a>
+    <a href="https://www.youtube.com/channel/UClgRkhTL3_hImCAmdLfDE4g" id="watchNow" class="btn btn-red" target="_blank">Watch Now</a></div>
 </div>
 
 `;
 }
+// onclick="youtubeMovie('${movie.Title}')"
+/////////////////////////////Watch Full Movie////////////////////////////////////////
+
+function youtubeMovie(title) {
+	console.log('1', title);
+	sessionStorage.setItem('movieTitle', title); //stores id locally in application tab session storage
+	getYoutubeMovie();
+	return false;
+}
+
+function getYoutubeMovie() {
+	console.log('2', 'get movie youtube');
+	let youtubeMovieTitle = sessionStorage.getItem('movieTitle');
+	const movieQuery = {
+		id: 'UClgRkhTL3_hImCAmdLfDE4g',
+		part: 'snippet, contentDetails, statistics',
+		key: 'AIzaSyDbDK_yKzFivQBrkukDw3lwBVsFgdDUmNY',
+		// AIzaSyBiFe0gSq_xIil6nwieRBdNbOuaI69AFnk created new key
+		fields: 'items/snippet/title' + youtubeMovieTitle
+	};
+
+	$.getJSON(YOUTUBE_MOVIE_URL, movieQuery, movieLink);
+}
+
+function movieLink(data) {
+	console.log('3', 'youtube movie data');
+	console.log('4', 'testtest', data);
+	const detailOfYoutubeMove = youtubeMoveResults(data);
+	$('#movie').html(detailOfYoutubeMove);
+}
+
+function youtubeMoveResults(movie) {
+	let youtubeMovieDetail = movie;
+	console.log('5', youtubeMovieDetail);
+	// console.log('6', youtubeMovieDetail.items[0].id.videoId);
+	return `
+	<div class="detail-row">
+		<iframe id="ytplayer" type="text/html" width="700px" height="500px" src="https://www.youtube.com/embed/${
+			youtubeMovieDetail.items[0].id.videoId
+		}?autoplay=1" frameborder='0'>
+		</iframe>
+		<div class="trailerButton">
+		<a href="index.html" class="btn">Back To Search</a>
+		</div>
+	</div>`;
+}
 
 ///////////////////Get Movie Trailer///////////////////////////
+function youtubeTrailer(title) {
+	console.log(title);
+	sessionStorage.setItem('trailerText', title); //stores id locally in application tab session storage
+	getTrailer();
+	return false;
+}
 
+function getTrailer() {
+	console.log('get trailer');
+	let trailer = sessionStorage.getItem('trailerText');
+	const trailerQuery = {
+		part: 'snippet',
+		key: 'AIzaSyDbDK_yKzFivQBrkukDw3lwBVsFgdDUmNY',
+		// AIzaSyBiFe0gSq_xIil6nwieRBdNbOuaI69AFnk created new key
+		q: trailer
+	};
+	$.getJSON(YOUTUBE_Trailer_URL, trailerQuery, trailerLink);
+}
 
+function trailerLink(data) {
+	console.log('trailer data');
+	const detailOfTrailer = trailerResults(data);
+	$('#movies').html(detailOfTrailer);
+}
 
+function trailerResults(trailer) {
+	let trailerDetail = trailer;
+	console.log(trailerDetail);
+	console.log(trailerDetail.items[0].id.videoId);
+	return `
+	<div class="detail-row">
+		<iframe id="ytplayer" type="text/html" width="700px" height="500px" src="https://www.youtube.com/embed/${
+			trailerDetail.items[0].id.videoId
+		}?autoplay=1" frameborder='0'>
+		</iframe>
+	</div>`;
+}
 
 $(watchSubmit);
