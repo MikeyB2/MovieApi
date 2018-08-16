063ccf2dc3674fce3802ceec52bcab3c3e7c6977
Mike <mike.bryant.jr@icloud.com>
2018-08-15 23:08:13 -0600

    added youtube trailer feature and new css

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-//  http://www.omdbapi.com?apikey=389ac1fa&s=
 const movie_SEARCH_URL = 'http://www.omdbapi.com?apikey=389ac1fa&';
-const ebay_SEARCH_URL = 'https://api.ebay.com/buy/browse/v1/item_summary/search?';
+const YOUTUBE_Trailer_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 let query = '';
 ///////////////////Get Initial search list of movies///////////////////////////
@@ -12,11 +11,9 @@ function watchSubmit() {
 		queryTarget.val('');
 		getDataForMovies(query, dataMovies);
 	});
-	console.log(query);
 }
 
 function getDataForMovies(searchText, callback) {
-	console.log('test3');
 	const query = {
 		s: `${searchText}`
 	};
@@ -24,38 +21,67 @@ function getDataForMovies(searchText, callback) {
 }
 
 function dataMovies(data) {
-	console.log('test5', data);
 	const movie = data.Search.map((item, index) => displayMovies(item));
 	$('#movies').html(movie);
 }
 
 function displayMovies(data) {
-	console.log(data);
 	let movie = returnResults(data);
-	console.log(movie);
 	return movie;
 }
 
 function returnResults(movie) {
 	return `
-        <div class="row">
           <div class="column">
-            <div class="card">
-                <img src="${movie.Poster}">
+                <img src="${movie.Poster}" class="img-list">
                 <h3>${movie.Title}</h3>
-                <a onclick="movieSelected('${movie.imdbID}', '${movie.Title} DVD')" class="btn btn-primary" href="#">Movie Details</a>
-            </div>
+								<a onclick="movieSelected('${movie.imdbID}')" class="btn" href="#">Movie Details</a>
+								<a onclick="youtubeTrailer('${movie.Title} Trailer')" class="btn" href="#">Watch Trailer</a>
           </div>
-        </div>
     `;
 }
+
+function youtubeTrailer(title) {
+	console.log(title);
+	sessionStorage.setItem('trailerText', title); //stores id locally in application tab session storage
+	window.location = 'movie.html';
+
+	return false;
+}
+
+function getTrailer() {
+	let trailer = sessionStorage.getItem('trailerText');
+	const trailerQuery = {
+		part: 'snippet',
+		key: 'AIzaSyDbDK_yKzFivQBrkukDw3lwBVsFgdDUmNY',
+		// AIzaSyBiFe0gSq_xIil6nwieRBdNbOuaI69AFnk created new key
+		q: trailer
+	};
+	$.getJSON(YOUTUBE_Trailer_URL, trailerQuery, trailerLink);
+}
+
+function trailerLink(data) {
+	console.log('test20', data);
+	const detailOfTrailer = trailerResults(data);
+	$('#movie').html(detailOfTrailer);
+	console.log('test8', detailOfTrailer);
+}
+
+function trailerResults(trailer) {
+	let trailerDetail = trailer;
+	console.log('test22', trailerDetail);
+	return `
+<div class="detail-row">
+<img src="${trailerDetail}" class="thumbnail">
+</div>`;
+}
+
 
 ///////////////////Get Detail of selected movie///////////////////////////
 
 function movieSelected(id, title) {
 	console.log(id, title);
 	sessionStorage.setItem('movieId', id); //stores id locally in application tab session storage
-	sessionStorage.setItem('movieTitle', title); //stores id locally in application tab session storage
 	window.location = 'movie.html';
 	return false;
 }
@@ -69,22 +95,27 @@ function getMovieDetails() {
 }
 
 function detailedMovie(data) {
-	console.log('test7', data);
 	const detailOfMovie = movieDetailResults(data);
 	$('#movie').html(detailOfMovie);
-	console.log('test8', detailOfMovie);
 }
 
 function movieDetailResults(movie) {
 	let movieDetail = movie;
-	console.log(movieDetail);
 	return `
 <div class="detail-row">
   <div class="details-card">
     <img src="${movieDetail.Poster}" class="thumbnail">
   </div>
   <div class="details-list">
-    <h2>${movieDetail.Title}</h2>
+		<h2>${movieDetail.Title}</h2>
+		<div class="plot-row">
+  <div class="plot">
+    <h3>Plot</h3>
+    ${movieDetail.Plot}
+    <hr>
+    
+  </div>
+</div>
     <ul class="list-group">
       <li class="list-group-item"><strong>IMDB Rating:</strong> ${
 				movieDetail.imdbRating
@@ -119,72 +150,21 @@ function movieDetailResults(movie) {
       <li class="list-group-item"><strong>Box Office:</strong> ${
 				movieDetail.BoxOffice
 			}</li>
-    </ul>
-  </div>
-</div>
-<div class="plot-row">
-  <div class="plot">
-    <h3>Plot</h3>
-    ${movieDetail.Plot}
-    <hr>
-    <a href="http://imdb.com/title/${
+		</ul>
+		<hr>
+	</div>
+	<div>
+	<a href="http://imdb.com/title/${
 			movieDetail.imdbID
-		}" target="_blank" class="btn btn-primary">View IMDB</a>
-    <a href="index.html" class="btn btn-default">Back To Search</a>
-    <a href="#" id="buyNow" class="btn btn-primary">Buy Now</a>
-  </div>
+		}" target="_blank" class="btn">View IMDB</a>
+    <a href="index.html" class="btn">Back To Search</a>
+    <a href="#" id="watchNow" class="btn btn-red">Watch Now</a></div>
 </div>
+
 `;
 }
 
-///////////////////Get Movie Detail results///////////////////////////
-
-
-function getEbayDetails() {
-	let movieTitle = sessionStorage.getItem('movieTitle');
-	const query = {
-		callname: 'FindProducts',
-		appid: 'MichaelB - movie - PRD - 72630 a405 - 65 a0dfb8',
-		q: movieTitle,
-		limit: '2',
-
-	};
-	$.getJSON(ebay_SEARCH_URL, query, buyMovieEbay);
-}
-
-function buyMovieEbay(data) {
-	console.log('test12', data);
-	const ebayMovies = ebayResults(data);
-	$('#movie').html(ebayMovies);
-	console.log('test18', ebayMovies);
-}
-
-function ebayResults(ebayData) {
-	let ebayDetail = ebayData;
-	console.log(ebayDetail);
-	return ``;
-}
-
-
-
-// goodreads apikey
-// key: FwUZbUbxGcRfnWpLfspzWA
-// secret: 2JgcP6U1d84aRcuZFQzLrwRK8DraeMurmsUC1Q0Wu8
-// "https://www.goodreads.com/search.xml?key=YOUR_KEY&q=Ender%27s+Game"
-
-// movieglu movie showtimes
-// https://developer.movieglu.com/api-index/quick-start-guide/ link to get started
-// API Endpoint :	https://api-gate.movieglu.com/
-// Username :	MOVI_18
-// API Key :	wWGXP95HyN9gg0jgE891C5DS2OuY5MXAarvKoLSL
-// Password :	QnbHvDERGQDd
-// Basic Authentication :	Basic TU9WSV8xODpRbmJIdkRFUkdRRGQ=
-// required fields below minus the geolocation
-// client: [Username]
-// x-api-key: [API Key]
-// Authorization: [Basic Authentication]
-// api-version: v102
-// Geolocation: lat;lng (Note the use of a semicolon. See documentation for further details)
+///////////////////Get Movie Trailer///////////////////////////
 
 
 
